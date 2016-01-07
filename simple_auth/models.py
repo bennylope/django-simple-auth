7383d4a8195db951e1eedf3f884d535f88196b1e
Ben Lopatin <ben@benlopatin.com>
2016-01-07 15:28:57 -0500

    Display name for password object

--- simple_auth/models.py
+++ simple_auth/models.py
@@ -8,3 +8,6 @@ from django.db import models
 class Password(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
