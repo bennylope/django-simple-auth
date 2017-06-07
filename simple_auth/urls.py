5fab8d52aaf2c1addc5dfa664490939ea9b21515
Ben Lopatin <ben@benlopatin.com>
2017-06-07 14:46:24 -0400

    Remove URL patterns

--- simple_auth/urls.py
+++ simple_auth/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import simple_password
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', view=simple_password, name="simple_auth_password"),
-)
+]
--- tests/urls.py
+++ tests/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import HttpResponse
 
@@ -9,10 +9,10 @@ def tester(request):
     return HttpResponse("Hello")
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^password/$', include('simple_auth.urls')),
     url(r'^tester/hello/$', view=tester),
     url(r'^goodbye/world/$', view=tester),
     url(r'^$', view=tester),
-)
+]
